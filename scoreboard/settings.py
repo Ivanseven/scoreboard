c7bf6b635d1f525d04a0668ee20791189059953a
IvanY <sir.ivanyeong@gmail.com>
2023-03-31 00:34:44 +0800

    Add debug toggle

--- scoreboard/settings.py
+++ scoreboard/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 from os import getenv
+from utils import get_env_var
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = getenv('DJANGO_SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = get_env_var('DEBUG', False)
 
 ALLOWED_HOSTS = [getenv('HOST_ENV')]
 
