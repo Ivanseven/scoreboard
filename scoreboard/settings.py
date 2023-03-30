2e800acc97d7be3eb9a2da6b0247cf6678e40b8d
IvanY <sir.ivanyeong@gmail.com>
2023-03-31 00:52:15 +0800

    Fix import

--- scoreboard/settings.py
+++ scoreboard/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 from os import getenv
-from utils import get_env_var
+from scoreboard.utils import get_env_var
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
