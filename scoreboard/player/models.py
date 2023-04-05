de09a87eb21ae33ebd298630a42630e6ea7d54f2
IvanY <sir.ivanyeong@gmail.com>
2023-04-06 01:21:46 +0800

    Fix player score method property

--- scoreboard/player/models.py
+++ scoreboard/player/models.py
@@ -7,7 +7,7 @@ class Player(User):
 
     @property
     def score(self):
-        score = self.score_set.aggregate(Sum('score'))['score__sum']
+        score = self.score_set.aggregate(Sum('value'))['value__sum']
         score = score if score != None else 0
         return score
 
