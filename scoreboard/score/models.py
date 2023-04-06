7db621db2d1870882bfbc9c758d12e74ce9a9303
IvanY <sir.ivanyeong@gmail.com>
2023-04-06 18:48:24 +0800

    Add fixtures for testing
    - remove TODO comment

--- scoreboard/score/models.py
+++ scoreboard/score/models.py
@@ -5,5 +5,5 @@ from scoreboard.player.models import Player
 class Score(models.Model):
     name = models.CharField(max_length=30)
     player = models.ForeignKey(Player, on_delete=models.CASCADE)
-    value = models.IntegerField(default=0) # TODO: rename this column to value in demo
+    value = models.IntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
